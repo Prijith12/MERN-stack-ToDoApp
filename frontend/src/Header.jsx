5c17504ff08040ea2b88a53782a283572b21a636
Prijith T <prijitht4@gmail.com>
2024-03-24 10:43:43 +0530

    frontend task adding input tag added, add button added , remove task functionality added, task completion status functionality added

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css';
+function Header() {
+  return (
+    <div className='bg-gradient-to-br from-purple-300 to-indigo-600'>
+     
+        <div>
+            <h1 className="text-center text-white text-4xl font-bold py-8">PriTechSolutions TO DO</h1>
+        </div>
+  
+
+    </div>
+  )
+}
+
+export default Header
